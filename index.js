6f57f8bd190490731848b47cdb5be902f4e58849
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-08 14:13:31 +0100

    fixed to 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ccx from 'cors'
 import { engine } from 'express-handlebars'
 var x = 1
 var y = Math.random()
+const rn = fix => Math.random().toFixed(fix)
 app()
   .engine('.hbs', engine())
   .set('view engine', '.hbs')
@@ -12,9 +13,9 @@ app()
   .use(ccx())
   .post('/post', (req, res) => {
     console.log(req.body)
-    res.send(JSON.stringify({x: ++x, y: Math.random()}))
+    res.send(JSON.stringify({x: ++x, y: rn(2)}))
   })
   .get('/', (req, res) => {
-    res.render('main', {a: 1, x: ++x, y: Math.random(), layout: false})
+    res.render('main', {a: 1, x: x++, y: rn(2), layout: false})
   })
   .listen(3000)
