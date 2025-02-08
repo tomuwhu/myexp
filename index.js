5322c82fdd5c47b93918de1fe15c300a23257a77
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-08 08:27:28 +0100

    .

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import app from 'express'
 import ccx from 'cors'
+import { engine } from 'express-handlebars'
+var x = 1
+var y = Math.random()
 app()
+  .engine('.hbs', engine({extname: '.hbs'}))
+  .set('view engine', '.hbs')
+  .set('views', './views')
   .use(app.json())
   .use(ccx())
-  .post('/', (req, res) => {
+  .post('/post', (req, res) => {
     console.log(req.body)
-    res.send(JSON.stringify({x: 1, y: 2}))
+    res.send(JSON.stringify({x: ++x, y: Math.random()}))
   })
   .get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('main', {a: 1, x: ++x, y: Math.random(), layout: false})
   })
   .listen(3000)
